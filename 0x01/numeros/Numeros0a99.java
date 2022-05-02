007c0a1586db53749e774fb0e9543ae107529f90
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-02 16:52:01 -0300

    Task 0 - Numeros 0 a 99

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -1,13 +1,12 @@
-package numeros;
 
 public class Numeros0a99 {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++){
             if(i < 99){
-                System.out.printf(i +",\n");
+                System.out.print(i +", ");
             }
             else{
-                System.out.println(i + "\n");
+                System.out.print(i + "\n" );
             }
         }
     }
