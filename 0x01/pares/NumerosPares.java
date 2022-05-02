fb496ba9fea1dfdb81b90b6cfc12011edf2a5783
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-02 17:05:45 -0300

    Task 1 - Numeros 0 a 100

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,15 @@
+//package pares;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 99; i++){
+            if(i % 2 ==0){
+                if(i < 98){
+                    System.out.print(i + ",");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
