ec61fe657bbbf2d699e610c345cb77fb63f36afe
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-05 16:59:44 -0300

    Task 3 - Retângulo Completo

---
+++ 0x05/retangulo_completo/Program.java
@@ -0,0 +1,14 @@
+
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(new Locale("pt", "BR"));
+        Retangulo retangulo = new Retangulo();
+        retangulo.setLargura(7);
+        retangulo.setAltura(4);
+
+        System.out.printf("Area: %.2f\n", retangulo.area());
+        System.out.println(retangulo.toString());
+    }
+}
