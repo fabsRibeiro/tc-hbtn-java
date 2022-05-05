f9e0ade692fb0243709b538ced63ff5c257dfa2a
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-05 11:32:07 -0300

    Task 3 - Status do personagem

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,53 @@
+
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int tomarDano(int quantidadeDeDano){
+        saudeAtual = getSaudeAtual() - quantidadeDeDano;
+        if(quantidadeDeDano > saudeAtual){
+            setSaudeAtual(0);
+        } else {
+            setSaudeAtual(saudeAtual);
+        }
+        return saudeAtual;
+    }
+
+    public int receberCura(int quantidadeDeCura){
+        saudeAtual = getSaudeAtual() + quantidadeDeCura;
+        if(getSaudeAtual() > 100){
+            setSaudeAtual(100);
+        } else {
+            setSaudeAtual(getSaudeAtual());
+        }
+
+        return saudeAtual;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        if(getSaudeAtual() > 0){
+            status = "vivo";
+        }
+        if(getSaudeAtual() <= 0){
+            status = "morto";
+        }
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
