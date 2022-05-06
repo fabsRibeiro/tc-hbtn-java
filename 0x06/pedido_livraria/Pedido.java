49ae6ecc45a6f8ff728029ba141bed43b276b04e
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-06 13:34:22 -0300

    Task 0 - Pedido Livraria

---
+++ 0x06/pedido_livraria/Pedido.java
@@ -0,0 +1,39 @@
+
+public class Pedido {
+
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double calcularTotal(){
+        ItemPedido[] itens = getItens();
+        double valorTotal  = 0;
+
+        for(int i = 0 ; i < getItens().length; i++){
+            double valor = itens[i].getProduto().obterPrecoLiquido() * itens[i].getQuantidade();
+            valorTotal = valorTotal + valor;
+        }
+
+        return valorTotal - (valorTotal * percentualDesconto/100);
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+
+    public void setItens(ItemPedido[] itens) {
+        this.itens = itens;
+    }
+}
