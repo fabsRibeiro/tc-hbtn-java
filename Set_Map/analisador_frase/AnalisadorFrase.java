f7aee5b7819ab11bcf980a08cf8ac7666a935647
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-19 14:00:23 -0300

    Task 0 - Contagem de Palavras

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -3,15 +3,17 @@ import java.util.TreeMap;
 
 public class AnalisadorFrase {
 
-    public static TreeMap contagemPalavras(String texto){
+    public static TreeMap<String,Integer> contagemPalavras(String texto){
         String[] palavras = texto.split("\\ ");
         TreeMap<String,Integer> palavrasSeparadas = new TreeMap<String,Integer>();
+
         for(String palavra1 : palavras){
             palavra1 = palavra1.toLowerCase();
             palavra1 = palavra1.replaceAll("\\?", "");
             palavra1 = palavra1.replaceAll("\\.", "");
             palavra1 = palavra1.replaceAll("\\!", "");
             int contador = 0;
+
             for(String palavra2 : palavras){
                 palavra2 = palavra2.toLowerCase();
                 palavra2 = palavra2.replaceAll("\\?", "");
