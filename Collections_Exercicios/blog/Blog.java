79a75f12ad3d8404078c5e7241fbc99afa590fcb
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-23 10:52:33 -0300

    Task 0 - Blog

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+
+public class Blog {
+
+    private ArrayList<Post> posts;
+
+    public Blog() {
+        this.posts = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post post){
+        posts.add(post);
+    }
+
+    public Set<String> obterTodosAutores(){
+        Set<String> autores = new TreeSet();
+        for(Post pots : posts){
+            autores.add(pots.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria(){
+        Map contagem = new TreeMap();
+        int devOps = 0;
+        int dev = 0;
+        for(Post post : posts){
+            if(post.getCategoria().equals("DevOps")){
+                devOps += 1;
+                if(devOps != 0){
+                    contagem.put(post.getCategoria(), devOps);
+                }
+            } else if(post.getCategoria().equals("Desenvolvimento")){
+                dev += 1;
+                if(dev != 0){
+                    contagem.put(post.getCategoria(), dev);
+                }
+            }
+        }
+        return contagem; //treemap
+    }
+}
