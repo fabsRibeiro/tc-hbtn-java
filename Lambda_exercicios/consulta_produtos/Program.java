612349903717198304e3bc88b0082df2d5855122
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-27 11:10:24 -0300

    Task 0 - Consultar Produtos

--- Lambda_exercicios/consulta_produtos/Program.java
+++ Lambda_exercicios/consulta_produtos/Program.java
@@ -1,9 +1,8 @@
 
-import markup.Produto;
+import Produto;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
+import java.util.function.*;
 
 public class Program {
     public static void main(String[] args) {
