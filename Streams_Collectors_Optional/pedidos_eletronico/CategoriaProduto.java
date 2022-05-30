f4fbbfb1034f84999f16cf6cc217eae1359afe3a
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-05-30 17:28:04 -0300

    Task 3 - Relatório Pedidos com Eletrônicos

---
+++ Streams_Collectors_Optional/pedidos_eletronico/CategoriaProduto.java
@@ -0,0 +1,8 @@
+
+
+public enum CategoriaProduto {
+
+    BRINQUEDO,
+    LIVRO,
+    ELETRONICO;
+}
---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,44 @@
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+
+        List<Produto> streamLista = pedido.getProdutos().stream()
+                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(toList());
+
+        return streamLista;
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produto){
+
+        Produto maiorValor = produto.stream()
+                .min(Comparator.comparing(Produto::getPreco).reversed()).get();
+
+        return maiorValor;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produto, int valor){
+
+        List<Produto> streamLista = produto.stream()
+                .filter(p -> p.getPreco() == valor || p.getPreco() > valor)
+                .collect(toList());
+
+        return streamLista;
+    }
+
+    public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos){
+
+        List<Pedido> listaPedido = pedidos.stream()
+                .filter(pedido -> pedido.getProdutos().stream().anyMatch(produto -> produto.getCategoria() == CategoriaProduto.ELETRONICO)).collect(toList());
+
+        return listaPedido;
+    }
+}
