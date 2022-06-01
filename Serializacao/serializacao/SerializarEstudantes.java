933ebb295c12d33f15db147afe8cda8dff539b97
fabsRibeiro <fabianoasribeiro@gmail.com>
2022-06-01 16:09:31 -0300

    Task 0 - Serialização de Estudantes

---
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -0,0 +1,44 @@
+
+
+import java.io.*;
+import java.util.List;
+
+public class SerializarEstudantes<Estudante> {
+
+    private String nomeArquivo;
+
+    public SerializarEstudantes(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void serializar(List<Estudante> estudantes){
+        try {
+            FileOutputStream fos = new FileOutputStream(nomeArquivo);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(estudantes);
+            oos.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Nao foi possivel serializar");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Estudante> desserializar(){
+        try {
+            FileInputStream fis = new FileInputStream(nomeArquivo);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            List<Estudante> estudantes = (List<Estudante>)ois.readObject();
+            ois.close();
+            return estudantes;
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
